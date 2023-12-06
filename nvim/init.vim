1cc1a859ad4de620ff2741b84e1688749d3b9f86
Michael Sanders <neoanomally@gmail.com>
2023-12-06 14:35:13 -0500

    Added some additional config changes for telescope

--- nvim/init.vim
+++ nvim/init.vim
@@ -34,11 +34,11 @@ Plug 'j-hui/fidget.nvim'
 call plug#end()
 
 " Using Lua functions
-nnoremap <leader>ff <cmd>lua require('telescope.builtin').find_files()<cr>
-nnoremap <leader>fg <cmd>lua require('telescope.builtin').live_grep()<cr>
-nnoremap <leader>fb <cmd>lua require('telescope.builtin').buffers()<cr>
-nnoremap <leader>fh <cmd>lua require('telescope.builtin').help_tags()<cr>
-nnoremap <leader>nt <cmd>NERDTree<cr>
+" nnoremap <leader>ff <cmd>lua require('telescope.builtin').find_files()<cr>
+" nnoremap <leader>fg <cmd>lua require('telescope.builtin').live_grep()<cr>
+" nnoremap <leader>fb <cmd>lua require('telescope.builtin').buffers()<cr>
+" nnoremap <leader>fh <cmd>lua require('telescope.builtin').help_tags()<cr>
+" nnoremap <leader>nt <cmd>NERDTree<cr>
 nnoremap <S-Tab> <<
 
 lua require('metals')
--- nvim/lua/neoanomally/main.lua
+++ nvim/lua/neoanomally/main.lua
@@ -12,6 +12,14 @@ require('mason').setup()
 vim.g.mapleader = ';'
 
 pcall(require('telescope').loadextension, 'fzf')
+require('telescope').setup({
+    defaults = {
+        file_ignore_patterns = { "^./.git/", "^node_modules", "target" },
+				layout_config = {
+					preview_cutoff = 10,
+				},
+    }
+})
 
 vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
 -- vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers { desc = '[ ] Find existing buffers' })
@@ -22,7 +30,7 @@ vim.keymap.set('n', '<leader>/', function()
 })
 end, { desc = '[/] Fuzzily search in current buffer]' })
 
-vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, {desc= '[S]earch [F]iles'})
+vim.keymap.set('n', '<leader>ff', require('telescope.builtin').find_files, {desc= '[S]earch [F]iles'})
 vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, {desc = '[S]earch [H]elp'})
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
